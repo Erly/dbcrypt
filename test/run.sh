e96fd78d8a1c8b0ac2d061076cd6bc4869c1e08d
Erlantz Oniga <erlantz@thesocialos.net>
2013-01-22 22:57:35 +0100

    Corrected a little bug with the location of the test in the run.sh

--- test/run.sh
+++ test/run.sh
@@ -7,10 +7,11 @@ DIR=$( cd $( dirname "${BASH_SOURCE[0]}" ) && pwd )
 
 # Note: dart_analyzer needs to be run from the root directory for proper path
 # canonicalization.
+echo $DIR/..
 pushd $DIR/..
 echo Analyzing library for warnings or type errors
 dart_analyzer --fatal-warnings --fatal-type-errors lib/dbcrypt.dart --work analyzer_out
 rm -r analyzer_out
 popd
 
-dart --checked dbcrypt_test.dart
+dart --checked $DIR/dbcrypt_test.dart
